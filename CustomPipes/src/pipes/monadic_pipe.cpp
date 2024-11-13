b66c413335bbee4ce7d148db467e69255a72605d
unknown <cyganek@agh.edu.pl>
2024-11-13 12:17:48 +0100

    updating project

---
+++ CustomPipes/src/pipes/monadic_pipe.cpp
@@ -0,0 +1,376 @@
+// ---------------------------------------------------
+// Created by Boguslaw Cyganek (C) 2024
+// ---------------------------------------------------
+
+
+
+
+
+#include <iostream>
+#include <array>
+#include <list>
+#include <vector>
+#include <string>
+
+#include <expected>
+
+#include <cassert>
+#include <format>
+#include <numeric>
+#include <algorithm>
+#include <filesystem>
+
+#include <iterator>
+#include <sstream>
+#include <fstream>
+#include <print>
+#include <tuple>
+
+#include <variant>
+#include <ranges>
+
+
+using namespace std::literals;
+
+namespace fs = std::filesystem;
+
+
+
+// ===================================================================
+// Version of the pipe-line with MONADIC std::expected
+
+
+
+
+namespace Monadic_VectorsPipeTest
+{
+
+
+	enum class ENormErr { kEmptyVec, kZeroSum, kWrongVals };
+
+	template < typename T, template < typename > typename Cont = std::vector >
+	using NormExpected = std::expected< Cont< T >, ENormErr >; 
+
+	// Takes an FP vector and returns its normalized E2 version
+	// Good only for floating-point types
+	template <	std::floating_point T, 
+					template < typename > typename Cont = std::vector, 
+					auto kThresh = 1e-76 >
+	auto normalize( Cont< T > v ) -> NormExpected< T, Cont >
+	{
+		if( not v.size() )
+			return std::unexpected( ENormErr::kEmptyVec );
+
+		auto denom = std::inner_product( v.begin(), v.end(), v.begin(), decltype( v )::value_type() );
+
+		if( denom < kThresh )	// check the denominator
+			return std::unexpected( ENormErr::kZeroSum );
+		else if( std::isinf( denom ) || std::isnan( denom ) )
+			return std::unexpected( ENormErr::kWrongVals );
+
+		std::transform( v.begin(), v.end(), v.begin(), [ sq = std::sqrt( denom ) ] ( auto x ) { return x / sq; } );
+		return v;
+	}
+
+
+	using DType = double;		
+	using DVec = std::vector< DType >;		
+	using VecOfVec = std::vector< DVec >;
+
+	using Matrix = VecOfVec;
+
+	using PathVec = std::vector< std::filesystem::path >;
+
+	enum class LoadErr { kNoData, kWrongPath, kCannotOpen, kWrongData };
+	using load_exp = std::expected< PathVec, LoadErr >;
+
+
+
+	using vec_vec_exp = std::expected< VecOfVec, ENormErr >;
+
+	enum class DistErr { kZeroLen, kWrongData };
+	using dist_exp = std::expected< Matrix, DistErr >;
+
+
+	enum class PathErr { kEmpty };		// no path provided
+	using path_exp = std::expected< std::filesystem::path, PathErr >;
+
+
+
+	using index_val = std::tuple< Matrix::size_type, Matrix::size_type, DType >;
+	using max_exp = std::expected< index_val, DistErr >;
+
+	using common_errors		= std::variant< PathErr, LoadErr, ENormErr, DistErr >;
+
+	using path_com_exp		= std::expected< std::filesystem::path,	common_errors >;
+	using load_com_exp		= std::expected< PathVec,						common_errors >;
+	using vec_vec_com_exp	= std::expected< VecOfVec,						common_errors >;
+	using dist_com_exp		= std::expected< Matrix,						common_errors >;
+	using max_com_exp			= std::expected< index_val,					common_errors >;
+
+
+	// traverse and collect all paths in this directory of files with the "accept_ext" extension
+	load_exp load_paths( std::filesystem::path && pe, const std::filesystem::path & accept_ext = "txt"sv )
+	{
+		if( ! fs::exists( pe ) || ! fs::is_directory( pe ) )
+			return std::unexpected( LoadErr::kWrongPath );		// exit if wrong path (not existing or not a dir)
+
+		// Iterate through the directory
+		load_exp retExp {};
+		for( const auto & file_obj : fs::directory_iterator( pe ) )
+				if( fs::is_regular_file( file_obj ) )	
+					if( file_obj.path().extension().string().contains( accept_ext.string() ) )
+						retExp->push_back( file_obj.path() );
+
+		return retExp->size() > 0 ? retExp : std::unexpected { LoadErr::kNoData };
+	}
+
+	load_com_exp load_paths_common( std::filesystem::path && pe, const std::filesystem::path & accept_ext = "txt" )
+	{
+
+		if( ! fs::exists( pe ) || ! fs::is_directory( pe ) )
+			return std::unexpected( LoadErr::kWrongPath );		// exit if wrong path (not existing or not a dir)
+
+		// Iterate through the directory
+		load_exp retExp {};
+		for( const auto & file_obj : fs::directory_iterator( pe ) )
+				if( fs::is_regular_file( file_obj ) )	
+					if( file_obj.path().extension().string().contains( accept_ext.string() ) )
+						retExp->push_back( file_obj.path() );
+
+		return retExp->size() > 0 ? retExp : std::unexpected { LoadErr::kNoData };
+	}
+
+
+
+
+
+	// open all files and read the vectors 
+	vec_vec_exp load_vectors( PathVec && le )
+	{
+		// open each file and read vectors
+		VecOfVec	retVecs;
+		for( const auto & f : le )
+		{
+			if( std::ifstream inFile( f ); inFile.is_open() )
+			{
+				for( std::string str; std::getline( inFile, str ) && str.length() > 0; )	// read the entire line into the string
+				{
+					std::istringstream istr( str );
+					using DType_Iter = std::istream_iterator< DType >;
+
+					retVecs.emplace_back(	std::make_move_iterator( DType_Iter{ istr } ), 
+													std::make_move_iterator( DType_Iter{} ) );
+				}
+			}
+		}
+
+		return retVecs.size() > 0 ? vec_vec_exp { retVecs } : std::unexpected( ENormErr::kEmptyVec );
+	}
+
+	vec_vec_com_exp load_vectors_common( PathVec && le )
+	{
+		// open each file and read vectors
+		VecOfVec	retVecs;
+		for( const auto & f :le )
+		{
+			if( std::ifstream inFile( f ); inFile.is_open() )
+			{
+				for( std::string str; std::getline( inFile, str ) && str.length() > 0; )	// read the entire line into the string
+				{
+					std::istringstream istr( str );
+					using DType_Iter = std::istream_iterator< DType >;
+
+					retVecs.emplace_back(	std::make_move_iterator( DType_Iter{ istr } ), 
+													std::make_move_iterator( DType_Iter{} ) );
+				}
+			}
+		}
+
+		return retVecs.size() > 0 ? vec_vec_exp { retVecs } : std::unexpected( ENormErr::kEmptyVec );
+	}
+
+	vec_vec_exp vec_normalize( VecOfVec && vve )	
+	{
+
+		for( auto && v : vve )
+			if( auto env = normalize< DVec::value_type, std::vector >( v ); env.has_value() )
+				v = env.value();
+			else
+				return std::unexpected( env.error() );		// stop immediately and pass the error out
+
+		return vve;
+	}
+
+	vec_vec_com_exp vec_normalize_common( VecOfVec && vve )	
+	{
+		for( auto && v : vve )
+			if( auto env = normalize< DVec::value_type, std::vector >( v ); env.has_value() )
+				v = env.value();
+			else
+				return std::unexpected( env.error() );		// stop immediately and pass the error out
+
+		return vve;
+	}
+
+
+	// Computes a cosine distance between vectors
+	// We assume that the input vectors are already normalized
+	dist_exp comp_distance( VecOfVec && vve )
+	{
+		const auto kColsRows { vve.size() };	// it's a square matrix
+		if( kColsRows == 0 )
+			return std::unexpected( DistErr::kZeroLen );
+
+		Matrix distances( kColsRows, DVec( kColsRows, DVec::value_type {} ) );
+
+		for( auto r { DVec::size_type {} }; r < kColsRows; ++ r )
+		{
+			const auto & v = ( vve )[ r ];
+			for( auto c { r + 1 }; c < kColsRows; ++ c )
+				distances[ r ][ c ] = std::inner_product( v.begin(), v.end(), ( vve )[ c ].begin(), std::remove_cvref_t< decltype(v) >::value_type {} );
+		}
+
+		return distances;
+	}
+
+
+	// Computes a cosine distance between vectors
+	// We assume that the input vectors are already normalized
+	dist_com_exp comp_distance_common( VecOfVec && vve )
+	{
+		const auto kColsRows { vve.size() };	// it's a square matrix
+		if( kColsRows == 0 )
+			return std::unexpected( DistErr::kZeroLen );
+
+		Matrix distances( kColsRows, DVec( kColsRows, DVec::value_type {} ) );
+
+		for( auto r { DVec::size_type {} }; r < kColsRows; ++ r )
+		{
+			const auto & v = ( vve )[ r ];
+			for( auto c { r + 1 }; c < kColsRows; ++ c )
+				distances[ r ][ c ] = std::inner_product( v.begin(), v.end(), ( vve )[ c ].begin(), std::remove_cvref_t< decltype(v) >::value_type {} );
+		}
+
+		return distances;
+	}
+
+
+	max_exp find_max( Matrix && de )
+	{
+		const auto kColsRows { de.size() };	// it's a square matrix
+		assert( kColsRows > 0 );
+		assert( (de)[ 0 ].size() > 0 );
+
+		constexpr DType kNoneVal { std::numeric_limits< DType >::lowest() };
+		index_val ret { 0u, 0u, kNoneVal };
+		for( Matrix::size_type r {}; r < kColsRows; ++ r )
+		{
+			for( Matrix::size_type c { r + 1 }; c < kColsRows; ++ c )
+			{
+				auto val = (de)[ r ][ c ];
+				assert( val >= -1.1 && val <= +1.1 );
+
+				if( std::get< 2 >( ret ) < val )
+					ret = { r, c, val };
+			}
+		}
+
+		return std::get< 2 >( ret ) != kNoneVal ? max_exp { ret } : std::unexpected( DistErr::kWrongData );
+	}
+
+	max_com_exp find_max_common( Matrix && de )
+	{
+		const auto kColsRows { de.size() };	// it's a square matrix
+		assert( kColsRows > 0 );
+		assert( (de)[ 0 ].size() > 0 );
+
+		constexpr DType kNoneVal { std::numeric_limits< DType >::lowest() };
+		index_val ret { 0u, 0u, kNoneVal };
+		for( Matrix::size_type r {}; r < kColsRows; ++ r )
+		{
+			for( Matrix::size_type c { r + 1 }; c < kColsRows; ++ c )
+			{
+				auto val = (de)[ r ][ c ];
+				assert( val >= -1.1 && val <= +1.1 );
+
+				if( std::get< 2 >( ret ) < val )
+					ret = { r, c, val };
+			}
+		}
+
+		return std::get< 2 >( ret ) != kNoneVal ? max_exp { ret } : std::unexpected( DistErr::kWrongData );
+	}
+
+
+	// https://en.cppreference.com/w/cpp/utility/variant/visit
+	// helper type for the visitor
+	template<class... Ts>
+	struct overloaded : Ts... { using Ts::operator()...; };
+
+
+	void GenPipeTest_Monadic()
+	{
+	
+		auto result =	path_com_exp( ".\\..\\data"sv )	
+							.and_then( [] ( auto && pe ) { return load_paths_common( std::move( pe ), "txt"sv ); } )
+							.and_then( load_vectors_common )
+							.and_then( vec_normalize_common )
+							.and_then( comp_distance_common )		
+							.and_then( find_max_common )
+
+							.and_then( [] ( auto && r )
+									{
+										auto [ x, y, v ] = r;
+										std::println( "success @ idx=({},{}; val={:.3f})\n", x, y, v );
+
+										return max_com_exp {};		
+									}
+								)
+
+							.or_else(	[] ( auto && err )
+									{
+										// err is a std::variant that holds different error types
+										std::visit(	overloaded {
+															[]( PathErr pe )	{ std::println( "PathErr #{}",	static_cast<int>(pe) ); },
+															[]( LoadErr le )	{ std::println( "LoadErr #{}",	static_cast<int>(le) ); },
+															[]( ENormErr ne ) { std::println( "ENormErr #{}",	static_cast<int>(ne) ); },
+															[]( DistErr de )	{ std::println( "DistErr #{}",	static_cast<int>(de) ); }
+														}, err );
+
+
+
+										return max_com_exp( std::unexpected( DistErr::kWrongData ) );		// propagate the error further on
+									}
+								)
+
+														;
+		std::println( "typeid( result ).name() == {}", typeid( result ).name() );
+
+
+
+		std::vector vals { 10, 11, 2, -3, 4, 55, 0, 0, -4, 10, -8 };
+
+		for( auto p : vals	| std::views::filter( []( auto a ){ return a > 0; } ) 
+									| std::views::transform( []( auto a ){ return 2 * a; } ) )
+				std::cout << p << ", ";
+
+
+		std::println( "\n\n" );
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CustomPipes/src/pipes/simple_custom_pipe_serial.cpp
@@ -0,0 +1,184 @@
+// ---------------------------------------------------
+// Created by Boguslaw Cyganek (C) 2024
+// ---------------------------------------------------
+
+
+
+#include <iostream>
+#include <ranges>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <format>
+#include <algorithm>
+#include <array>
+#include <numeric>
+#include <cassert>
+#include <any>
+#include <optional>
+#include <functional>
+#include <ctime>
+
+#include <expected>
+#include <iomanip>			// for std::quoted
+
+#include "helpers.h"
+#include "range.h"
+
+#include <random>
+#include <print>
+
+
+import payload;
+
+
+
+// ===================================================================
+// Version of the pipe-line with std::expected
+
+template < typename T >
+concept is_expected = requires( T t )
+{
+	typename T::value_type;		// type requirement – nested member name exists
+	typename T::error_type;		// type requirement – nested member name exists
+
+	requires std::is_constructible_v< bool, T >;		// std::convertible_to< T, bool > will not work - ok, there is a conversion but this is a CONTEXTUAL CONVERSION!
+	requires std::same_as< decltype( * t ), typename T::value_type & >;
+	requires std::same_as< std::remove_cvref_t< decltype( * t ) >, typename T::value_type >;
+
+	requires std::constructible_from< T, std::unexpected< typename T::error_type > >; 
+};
+
+
+#if 0
+// In this version if there is an error in the pipeline, then the further functions in the chaing are NOT called at all
+template < typename T, typename E, typename Function >
+requires std::invocable< Function, std::expected< T, E > >
+			&& is_expected< typename std::invoke_result_t< Function, std::expected< T, E > > >
+constexpr auto operator | ( std::expected< T, E > && ex, Function && f ) -> typename std::invoke_result_t< Function, std::expected< T, E > >
+{
+	return ex ? std::invoke( std::forward< Function >( f ), * std::forward< std::expected< T, E > >( ex ) ) : ex;
+}
+
+#else
+
+// The second, more general version allows passing in and out std::expected with possibly different types
+// In this version all functions in the pipe are called and there is their responsibility to check if the passed object has and object or an error
+template < typename InT, typename InE, typename Function >
+requires std::invocable< Function, std::expected< InT, InE > >
+			&& is_expected< typename std::invoke_result_t< Function, std::expected< InT, InE > > >
+constexpr auto operator | ( std::expected< InT, InE > && ex, Function && f ) -> typename std::invoke_result_t< Function, std::expected< InT, InE > >
+{
+	return std::invoke( std::forward< Function >( f ), /***/ std::forward< std::expected< InT, InE > >( ex ) );
+}
+#endif 
+
+
+PayloadOrError Payload_Proc_1( PayloadOrError && s )
+{
+	if( ! s )
+		return s;
+	++ s->fVal;
+	s->fStr += " proc by 1,";
+	std::cout << "I'm in Payload_Proc_1, s = " << s->fStr << "\n";
+	return s;
+}
+
+PayloadOrError Payload_Proc_2( PayloadOrError && s )
+{
+	if( ! s )
+		return s;
+	++ s->fVal;
+	s->fStr += " proc by 2,";
+	std::cout << "I'm in Payload_Proc_2, s = " << s->fStr << "\n";
+
+	// Emulate the error, at least once in a while ...
+	std::mt19937 rand_gen( std::random_device {} () );
+	return ( rand_gen() % 2 ) ? s : 
+					std::unexpected { rand_gen() % 2 ? OpErrorType::kOverflow : OpErrorType::kUnderflow };
+}
+
+PayloadOrError Payload_Proc_3( PayloadOrError && s )
+{
+	if( ! s )
+		return s;
+	++ s->fVal;
+	s->fStr += " proc by 3,";
+	std::cout << "I'm in Payload_Proc_3, s = " << s->fStr << "\n";
+	return s;
+}
+
+
+void Payload_PipeTest()
+{
+	static_assert( is_expected< PayloadOrError > );	// a short-cut to verify the concept
+
+	// ---
+	PayloadOrError	pe { Payload { "just to test it", 13 } };		// to test with and without "*" in operator |
+	auto cos = * pe;
+	PayloadOrError second_pe = cos;
+	// ---
+
+	// ----------------------------------------------------------------------------------------------------------------
+	// Here we create our CUSTOM PIPE
+	auto res = 	PayloadOrError { Payload { "Start string ", 42 } } | Payload_Proc_1 | Payload_Proc_2 | Payload_Proc_3 ;
+	// ----------------------------------------------------------------------------------------------------------------
+	 
+	if( res )
+		print_nl( "Success! Result of the pipe: fStr == ", res->fStr, " fVal == ", res->fVal );
+	else
+		switch( res.error() )
+		{
+			case OpErrorType::kInvalidInput: print_nl( "Error: OpErrorType::kInvalidInput" ); break;
+			case OpErrorType::kOverflow:		print_nl( "Error: OpErrorType::kOverflow" );		break;
+			case OpErrorType::kUnderflow:		print_nl( "Error: OpErrorType::kUnderflow" );		break;
+			default: print_nl( "That's really an unexpected error ..." );							break;
+		}
+}
+
+
+// This is a version of Payload_PipeTest but this time with the monadic interface of std::expected
+void Payload_PipeTest_Monadic()
+{
+
+	auto test = Payload_Proc_1( Payload { "Start string ", 42 } );
+
+	auto res = 	PayloadOrError { Payload { "Start string ", 42 } }
+
+		.and_then( Payload_Proc_1 )
+		.and_then( Payload_Proc_2 )
+		.and_then( Payload_Proc_3 )
+		
+		.or_else(	[]( const auto & err )
+						{
+							switch( err )
+							{
+								case OpErrorType::kInvalidInput: print_nl( "Error: OpErrorType::kInvalidInput" );	break;
+								case OpErrorType::kOverflow:		print_nl( "Error: OpErrorType::kOverflow" );			break;
+								case OpErrorType::kUnderflow:		print_nl( "Error: OpErrorType::kUnderflow" );		break;
+								default: print_nl( "That's really an unexpected error ..." );								break;
+							}
+							return PayloadOrError( std::unexpected( err ) );		// propagate the error further on
+							//return PayloadOrError( Payload() );		// but we can also fix the situation and return a value
+						}
+		);
+
+
+		print_nl( "typeid( res ).name() == ", typeid( res ).name() );
+
+		if( res )
+			print_nl(  "Success! Result of the pipe: fStr == ", res->fStr, " fVal == ", res->fVal );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
